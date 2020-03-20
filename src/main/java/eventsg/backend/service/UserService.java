8326f82f9a6843f741e6140605d0abeba5e52274
Chen Xinyi <xchen032@e.ntu.edu.sg>
2020-03-20 22:27:20 +0800

    Added UserService

--- src/main/java/eventsg/backend/service/UserService.java
+++ src/main/java/eventsg/backend/service/UserService.java
@@ -1,4 +1,60 @@
 package eventsg.backend.service;
 
-public interface UserService {
+import eventsg.backend.dao.UserDao;
+import eventsg.backend.model.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class UserService {
+    private final UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    int insertUser(User user){
+        return userDao.insertUser(user);
+    };
+
+    Optional<UUID> login(String email, String password){
+        return userDao.login(email, password);
+    };
+
+    Optional<User> getUserById(UUID id){
+        return userDao.getUserById(id);
+    };
+
+    int updateUserById(UUID id, User user){
+        return userDao.updateUserById(id, user);
+    };
+
+    int deleteUserById(UUID id){
+        return deleteUserById(id);
+    };
+
+    int addInterestedCategory (UUID userid, String category){
+        return addInterestedCategory(userid, category);
+    };
+
+    int deleteInterestedCategory (UUID userid, String category){
+        return deleteInterestedCategory(userid, category);
+    };
+
+    List<String> getInterestedCategories(UUID userid){
+        return getInterestedCategories(userid);
+    };
+
+    int saveEvent(UUID userid, UUID eventid){
+        return saveEvent(userid, eventid);
+    };
+
+    int unsaveEvent(UUID userid, UUID eventid){
+        return unsaveEvent(userid, eventid);
+    };
+
+    List<UUID> getSavedEvents(UUID userId){
+        return getSavedEvents(userId);
+    };
 }
