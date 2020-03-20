87400e824ea2a9b03115943eb62b1dcedf137fa8
Chen Xinyi <xchen032@e.ntu.edu.sg>
2020-03-20 22:20:35 +0800

    Added UserDataAccessService(postgres)

--- src/main/java/eventsg/backend/dao/UserDao.java
+++ src/main/java/eventsg/backend/dao/UserDao.java
@@ -1,4 +1,36 @@
 package eventsg.backend.dao;
 
+import eventsg.backend.model.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserDao {
+
+    int insertUser(User user);
+
+    // Basic account operations
+    Optional<UUID> login(String email, String password);
+
+    Optional<User> getUserById(UUID id);
+
+    int updateUserById(UUID id, User user);
+
+    int deleteUserById(UUID id);
+
+    // Interested categories
+    int addInterestedCategory (UUID userid, String category);
+
+    int deleteInterestedCategory (UUID userid, String category);
+
+    List<String> getInterestedCategories(UUID userid);
+
+    // Saved events
+    int saveEvent(UUID userid, UUID eventid);
+
+    int unsaveEvent(UUID userid, UUID eventid);
+
+    List<UUID> getSavedEvents(UUID userId);
+
 }
--- src/main/java/eventsg/backend/model/User.java
+++ src/main/java/eventsg/backend/model/User.java
@@ -5,23 +5,70 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * This class represents a user account.
+ */
 public class User {
 
+    /**
+     * Unique user ID assigned to user when account is created.
+     */
     private final UUID userId;
+
+    /**
+     * Username set by user which should be unique.
+     */
     private String userName;
+
+    /**
+     * First name of user.
+     */
+    private String firstName;
+
+    /**
+     * Last name of user.
+     */
+    private String lastName;
+
+    /**
+     * Email address of the account which should be unique.
+     */
     private String email;
+
+    /**
+     * password set by user.
+     */
     private String password;
+
+    /**
+     * Date of birth of user.
+     */
     private LocalDate birthday;
+
+    /**
+     * Phone number of user which should be unique.
+     */
     private int phoneNum;
+
+    /**
+     * Occupation of user.
+     */
     private String occupation;
+
+    /**
+     * Organization the user belongs to.
+     */
     private String organization;
 //    private List<String> interestedEventCategories; // separate table needed: userid vs category
 //    private List<UUID> savedEvents; // separate table needed: userid vs event id
 
-    public User(UUID userId, String userName, String email, String password, LocalDate birthday, int phoneNum,
-                String occupation, String organization) {
+
+    public User(UUID userId, String userName, String firstName, String lastName, String email, String password,
+                LocalDate birthday, int phoneNum, String occupation, String organization) {
         this.userId = userId;
         this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.birthday = birthday;
@@ -62,5 +109,13 @@ public class User {
 
     public String getOrganization() {
         return organization;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }
